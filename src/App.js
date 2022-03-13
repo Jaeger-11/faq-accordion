73913d747ad92a39926b50ff18c47151b71c9772
Falodun Damilola <dpaulzs11@gmail.com>
2022-03-13 17:51:07 +0100

    CHANGES MADE

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import bg from './illustration-woman-online-desktop.svg'
 import box from './images/illustration-box-desktop.svg'
 
 const App = () => {
-  // const bgc = {background: 'linear-gradient(to right, rgb(175, 103, 233),rgb(101, 101, 231))'}
+  const bgc = {
+    background: 'linear-gradient(to right, rgb(101, 101, 231), rgb(175, 103, 233))'
+  }
   return (
-    <div>
+    <div className='app' style={bgc}>
       <img src={box} alt = 'box' className='box'/>
       <div className='container'>
         <section className='imgSection'>
@@ -20,7 +22,11 @@ const App = () => {
           })}
         </main>
       </div>
+      <div className="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href='https://github.com/Jaeger-11'> Falodun Oluwadamilola </a>.
     </div>
+  </div>
   );
 }
 
